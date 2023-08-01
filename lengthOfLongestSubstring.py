25b0f49267cae7a397eb68fa6111c035d227173e
Felix Boachie Yiadom <94701949+FelixNotter@users.noreply.github.com>
2023-08-01 21:18:20 +0000

    Create lengthOfLongestSubstring.py

---
+++ lengthOfLongestSubstring.py
@@ -0,0 +1,15 @@
+class Solution:
+    def lengthOfLongestSubstring(self, s: str) -> int:
+        length = 0
+        l = 0
+        seen = set()
+        for r in range(len(s)):
+            while s[r] in seen:
+                seen.remove(s[l])
+                l+=1
+            seen.add(s[r])
+            length = max(length,len(seen))
+        return length 
+
+        
+        
