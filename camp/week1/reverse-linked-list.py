1c2f9898ee243c50da877be86f124ca801c583b4
Felix Boachie Yiadom <94701949+FelixNotter@users.noreply.github.com>
2023-11-08 20:53:41 +0000

    Added solution for Reverse Linked List

--- camp/week1/reverse-linked-list.py
+++ camp/week1/reverse-linked-list.py
@@ -4,25 +4,13 @@
 #         self.val = val
 #         self.next = next
 class Solution:
-    def reverseList(self, head: Optional[ListNode],prev = None) -> Optional[ListNode]:
-        # #Empty list base case
-        # if head == None:
-        #     return None
-        # #initialising iterating variables
-        # current = head
-        # prev = None
-        # next1 = current.next
-        # #Visiting and rerouting Nodes
-        # while(next1 != None):
-        #     current.next = prev
-        #     prev = current
-        #     current = next1
-        #     next1 = next1.next
-        # #Rerouting New head node and returning it
-        # current.next = prev
-        # return current
-        if not head:return prev
-        current = head.next
-        head.next = prev
-        prev = head
-        return self.reverseList(current,prev)
+    def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        def dfs(node,prev):
+            if not node:
+                return prev
+            cur = node.next
+            node.next = prev
+        
+            return dfs(cur,node)
+        return dfs(head,None)
+        
