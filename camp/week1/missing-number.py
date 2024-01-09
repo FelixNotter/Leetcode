028e0d103554fd5ec7225a66d81b6915f5f2674f
Felix Boachie Yiadom <94701949+FelixNotter@users.noreply.github.com>
2024-01-09 18:37:13 +0000

    Added solution for Missing Number

---
+++ camp/week1/missing-number.py
@@ -0,0 +1,15 @@
+class Solution:
+    def missingNumber(self, nums: List[int]) -> int:
+        nums.append(-1)
+        i = 0
+        while i < len(nums):
+            if nums[i] != i:
+                if nums[i] == -1:
+                    ans = i
+                    i+=1
+                else:
+                    nums[nums[i]],nums[i] = nums[i],nums[nums[i]]
+            else:
+                i+=1
+        return ans
+        
