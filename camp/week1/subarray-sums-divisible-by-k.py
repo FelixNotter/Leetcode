58bc6f3e7e708fc3ea61bccd07dd021be127c22e
Felix Boachie Yiadom <94701949+FelixNotter@users.noreply.github.com>
2024-03-19 21:52:37 +0000

    Added solution for Subarray Sums Divisible by K

--- camp/week1/subarray-sums-divisible-by-k.py
+++ camp/week1/subarray-sums-divisible-by-k.py
@@ -2,11 +2,12 @@ class Solution:
     def subarraysDivByK(self, nums: List[int], k: int) -> int:
         count = defaultdict(int)
         count[0] = 1
-        res =  0
-        cur = 0 
-        for r in range(len(nums)):
-            cur += nums[r]
-            cur%=k
-            res += count[cur]
-            count[cur] += 1
+        total = 0
+        res = 0
+        for i in range(len(nums)):
+            total+=nums[i]
+            res+=count[total%k]
+            count[total%k]+=1
         return res
+            
+        
